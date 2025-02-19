f806d923e18c8276d6bb83f87b03a60631ddf516
Khahaha10 <knurfattah@gmail.com>
2025-02-19 16:07:15 +0700

    feat; implement Shadcn in read user by id page #6

--- next-frontend/src/app/users/read/[id]/page.tsx
+++ next-frontend/src/app/users/read/[id]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { getUserById } from "@/app/utils/queries/users/[id]/route";
+import { Card, CardContent } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
 import { use } from "react";
 
@@ -15,17 +16,26 @@ export default function Detail({ params }: { params: Promise<{ id: number }> }) 
     if (!user) return <div><span>No user found</span></div>;
 
     return (
-        <div className='w-full'>
-            <h2 className='text-center font-bold text-3xl py-3'>{user.username}</h2>
-            <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p >{user.name}</p>
-            </div>
-            <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p >{user.address}</p>
-            </div>
-            <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p >{user.phone}</p>
-            </div>
+        <div className='w-full flex flex-col items-center space-y-4 py-6'>
+            <h2 className='text-center font-bold text-3xl'>{user.username}</h2>
+            <Card className='w-full max-w-4xl'>
+                <CardContent>
+                    <b>Name</b>
+                    <p>{user.name}</p>
+                </CardContent>
+            </Card>
+            <Card className='w-full max-w-4xl'>
+                <CardContent>
+                    <b>Address</b>
+                    <p>{user.address}</p>
+                </CardContent>
+            </Card>
+            <Card className='w-full max-w-4xl'>
+                <CardContent>
+                    <b>Phone</b>
+                    <p>{user.phone}</p>
+                </CardContent>
+            </Card>
         </div>
     );
 }
