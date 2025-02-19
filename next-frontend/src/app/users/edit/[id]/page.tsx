c1d8cd3b5ecd2c4e158d80d60c72939ef0ce3835
Khahaha10 <knurfattah@gmail.com>
2025-02-19 11:24:52 +0700

    feat: implement TanStack Query in update user page #3 #6

--- next-frontend/src/app/users/edit/[id]/page.tsx
+++ next-frontend/src/app/users/edit/[id]/page.tsx
@@ -1,19 +1,26 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import React, { useState, useEffect, use } from "react";
 import { useRouter } from "next/navigation";
-import { fetcher } from "@/app/libs";
-import useSWR from "swr";
+import { useQuery, useMutation, QueryClient } from "@tanstack/react-query";
+import { getUserById, updateUser } from "@/app/utils/queries/users/[id]/route";
 
-export default function PostEdit({ params }: { params: Promise<{ id: number }> }) {
+export default function UserEdit({ params }: { params: Promise<{ id: number }> }) {
+    const queryClient = new QueryClient()
+    
     const router = useRouter();
-    const resolvedParams = use(params);
-    const { data: user, isLoading, error } = useSWR(`/utils/queries/users/${resolvedParams.id}`, fetcher);
+    const { id: userId } = use(params);
 
-    const [username, setUsername] = useState<string>("");
-    const [name, setName] = useState<string>("");
-    const [address, setAddress] = useState<string>("");
-    const [phone, setPhone] = useState<string>("");
+    const { data: user} = useQuery({
+        queryKey: ["user", userId],
+        queryFn: () => getUserById({ id: userId })
+    });
+
+    const [username, setUsername] = useState("");
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [phone, setPhone] = useState("");
 
     useEffect(() => {
         if (user?.result) {
@@ -22,75 +29,54 @@ export default function PostEdit({ params }: { params: Promise<{ id: number }> }
             setAddress(user.result.address);
             setPhone(user.result.phone);
         }
-    }, [user, isLoading]);
+    }, [user]);
 
-    const updateUser = async (e: React.FormEvent) => {
+    const mutation = useMutation({
+        mutationFn: (data: any) => updateUser({ id: userId }, data),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['todos'] });
+            router.push("/users");
+        },
+    });
+
+    const editUser = (e: React.FormEvent) => {
         e.preventDefault();
-        if (username !== "" && name !== "" && address !== "" && phone !== "") {
-            const formData = { username, name, address, phone };
+        if (username && name && address && phone) {
+            const userData = { 
+                username, 
+                name, 
+                address, 
+                phone 
+            };
 
-            const res = await fetch(`/utils/queries/users/${resolvedParams.id}`, {
-                method: "PUT",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(formData),
-            });
-
-            const content = await res.json();
-            if (content.success > 0) {
-                router.push("/users");
-            }
+            mutation.mutate(userData);
         }
     };
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error loading user data.</div>;
-    if (!user) return <div>Error page.</div>;
+    if (!user) return <div>User not found.</div>;
 
     return (
         <div className="w-full max-w-7xl m-auto">
-            <form className="w-full" onSubmit={updateUser}>
+            <form className="w-full" onSubmit={editUser}>
                 <span className="font-bold text-yellow-500 py-2 block underline text-2xl">Edit User</span>
                 <div className="w-full py-2">
                     <label className="text-sm font-bold py-2 block">Username</label>
-                    <input
-                        type="text"
-                        name="username"
-                        className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+                    <input type="text" className="w-full border-[1px] border-gray-200 p-2 rounded-sm" value={username} onChange={(e) => setUsername(e.target.value)}/>
                 </div>
                 <div className="w-full py-2">
                     <label className="text-sm font-bold py-2 block">Name</label>
-                    <input
-                        type="text"
-                        name="name"
-                        className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
+                    <input type="text" className="w-full border-[1px] border-gray-200 p-2 rounded-sm" value={name} onChange={(e) => setName(e.target.value)}/>
                 </div>
                 <div className="w-full py-2">
                     <label className="text-sm font-bold py-2 block">Address</label>
-                    <textarea
-                        name="address"
-                        className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
-                        value={address}
-                        onChange={(e) => setAddress(e.target.value)}
-                    />
+                    <textarea className="w-full border-[1px] border-gray-200 p-2 rounded-sm" value={address} onChange={(e) => setAddress(e.target.value)}/>
                 </div>
                 <div className="w-full py-2">
                     <label className="text-sm font-bold py-2 block">Phone</label>
-                    <input
-                        type="text"
-                        name="phone"
-                        className="w-full border-[1px] border-gray-200 p-2 rounded-sm"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                    />
+                    <input type="text" className="w-full border-[1px] border-gray-200 p-2 rounded-sm" value={phone} onChange={(e) => setPhone(e.target.value)}/>
                 </div>
                 <div className="w-full py-2">
-                    <button className="w-20 p-2 text-white border-gray-200 border-[1px] rounded-sm bg-green-400">
+                    <button type="submit" className="w-20 p-2 text-white border-gray-200 border-[1px] rounded-sm bg-green-400">
                         Update
                     </button>
                 </div>
